c13b5ca6f2ef8f68beb4c6a03f1ae10c2d4cec97
Mandeep Sai <bandimandeep7@gmail.com>
2022-12-20 15:38:54 +0100

    feat: display movies in gallery as carousel

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,13 +1,59 @@
-import { GalleryProps } from "../types";
+import { useEffect, useState } from "react";
+import { GalleryProps, Movie } from "../types";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./Gallery.css";
 
 const Gallery = ({ category, movies }: GalleryProps): JSX.Element => {
+  const [genreMovies, setGenreMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genres.includes(category)
+    );
+    setGenreMovies(filteredMovies);
+  }, [movies, category]);
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1500 },
+      items: 6,
+    },
+    largeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 1500, min: 1300 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 1400, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
-    <>
-      <h1>
-        {category}
-        {movies.length}
-      </h1>
-    </>
+    <div className="gallery">
+      <h3>{category}</h3>
+      <Carousel responsive={responsive}>
+        {genreMovies.map((movie) => {
+          return (
+            <div>
+              <img
+                src={movie.poster}
+                style={{ height: "350px", width: "250px" }}
+                alt=""
+              />
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
   );
 };
 
