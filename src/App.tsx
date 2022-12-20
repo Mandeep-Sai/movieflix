fbc5b528ea5122ee7dad225c9958b4b3e9aed7b0
Mandeep Sai <bandimandeep7@gmail.com>
2022-12-20 12:41:59 +0100

    feat: create home component to display movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/Home/Home";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
+      <Home></Home>
     </div>
   );
 }
