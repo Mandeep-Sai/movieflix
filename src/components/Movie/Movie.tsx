fa873fa7f7b13d2d294b1ebf11d4c05c8f50376d
Mandeep Sai <bandimandeep7@gmail.com>
2022-12-20 21:42:03 +0100

    feat: populate movie component with data as in design

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,9 +1,56 @@
-import { MovieAndHomeProps } from "../types";
+import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { Movie as MovieType, MovieAndHomeProps } from "../types";
+import "./Movie.css";
 
 const Movie = ({ data }: MovieAndHomeProps): JSX.Element => {
+  const [movieData, setMovieData] = useState<MovieType>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const { id } = useParams();
+
+  const transformDateToYear = (date: Date) => {
+    let d = new Date(date);
+    return d.getFullYear();
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    setMovieData(data.find((movie) => movie.id === id));
+    setLoading(false);
+  }, [data]);
   return (
     <>
-      <h1>{data.length}</h1>
+      {loading ? (
+        <Spinner
+          animation="border"
+          variant="light"
+          style={{ position: "absolute", top: "50%", left: "50%" }}
+        />
+      ) : (
+        <div className="movie_container">
+          <div>
+            <img src={movieData?.poster} alt="" />
+          </div>
+          <div className="info">
+            <div className="title_container">
+              <div className="title">
+                <h4>{movieData?.title}</h4>
+                <h4>({movieData?.imdb_rating}/10)</h4>
+              </div>
+              <div className="rating">Stars</div>
+            </div>
+
+            <p className="date">
+              {movieData?.released_on &&
+                transformDateToYear(movieData?.released_on)}{" "}
+              | {movieData?.director} | {movieData?.length}
+            </p>
+            <p className="cast">Cast : {movieData?.cast.join(" , ")}</p>
+            <p>Description: {movieData?.overview}</p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
