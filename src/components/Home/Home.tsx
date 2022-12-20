e861f41d734195cb2de4fb811504cac2f97635fb
Mandeep Sai <bandimandeep7@gmail.com>
2022-12-20 13:07:49 +0100

    feat: fetch movies from api on mounting home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Movie } from "../types";
+
 const Home = (): JSX.Element => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const fetchMovies = async (url: string): Promise<Movie[]> => {
+    return await axios
+      .get(url, {
+        headers: {
+          Authorization: "Bearer Wookie2021",
+        },
+      })
+      .then((response) => response.data.movies)
+      .catch((error) => console.log(`Error : ${error}`));
+  };
+
+  useEffect(() => {
+    fetchMovies("https://wookie.codesubmit.io/movies").then((movies) =>
+      setMovies(movies)
+    );
+  }, []);
   return (
     <>
-      <h1>Home</h1>
+      <h1>Number of movies : {movies.length}</h1>
     </>
   );
 };
